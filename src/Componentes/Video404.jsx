d92b6b7c6d71f38362ab10ce6a2493d2006749f1
José Daniel Mantilla Franqui <danielfranqui016@gmail.com>
2024-04-25 14:07:13 -0500

    Modificado componente de la página 404

--- src/Componentes/Video404.jsx
+++ src/Componentes/Video404.jsx
@@ -1,21 +1,69 @@
 import styled from 'styled-components'
-import video404 from '../assets/video/video404.mp4'
-
-const StyledVideo404 = styled.video`
-    width:100%;
-`
 
 const StyledContainer = styled.section`
-    @media (max-width:900px) {
-        background: black;
-        padding: 15rem 0;
-    }
+  width: 100%;
+  height:40vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url('/background-404.gif');
+  background-color: #908d8d;
+  background-blend-mode: soft-light;
+  background-position: center;
+
+  @media screen and (max-width:480px) {
+    height: 200vw;
+    width: 100vw;
+  }
+
+  @media screen and (min-width:481px) and (max-width: 999px) {
+    width: 100%;
+    height: 150vw;
+  }
+`
+
+const ErrorContainer = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  background-color: #fdfdfd8f;
+  backdrop-filter: blur(3px);
+  border-radius: 30px;
+  @media screen and (max-width:480px) {
+    width: 90%
+  }
+
+  @media screen and (min-width:481px) and (max-width: 999px) {
+    width: 70%;
+  }
+`
+
+const ErrorCodigo = styled.p`
+  color: #353232;
+  font-size: 3rem;
+  font-weight: 700;
+
+`
+
+const ErrorDescripcion = styled.p`
+  color: #353535;
+  font-size: 2rem;
+  font-weight: 700;
+  text-align: center;
+
+
 `
 
 const Video404 = () => {
   return (
     <StyledContainer>
-      <StyledVideo404 src={video404} autoPlay muted loop />
+      <ErrorContainer>
+        <ErrorCodigo>Error 404</ErrorCodigo>
+        <ErrorDescripcion>El recurso solicitado no existe.</ErrorDescripcion>
+      </ErrorContainer>
     </StyledContainer>
   )
 }
